aed0d504730e99aabb837342fc502456680a612f
shweba sheikh <sheikhshweba@gmail.com>
2026-06-19 21:39:07 +0530

    dark mode added

--- script.js
+++ script.js
@@ -82,5 +82,10 @@ const mode = document.getElementById("mode");
 mode.addEventListener("click", () => {
 
     document.body.classList.toggle("dark");
+    if (document.body.classList.contains("dark")) {
+        mode.innerHTML = "☀️ Light Mode";
+    } else {
+        mode.innerHTML = "🌙 Dark Mode";
+    }
 
 });
